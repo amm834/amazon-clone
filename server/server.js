000f1ae726bc373055107bd09772da69a84ce2a1
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-24 00:56:27 +0630

    Server get and post

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(morgan('dev'))
+app.use(bodyParser.json({
+    extended: true
+}))
+
+app.get('/', (req, res) => {
+    res.json({
+        'message': 'Hello World'
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+})
+
+server.listen(3000, () => {
+    console.log('Server is running')
+})
