80859daa9ec87ef137ffdcb6b89e44dad122f8fd
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-26 13:41:24 +0630

    -m

--- server/server.js
+++ server/server.js
@@ -2,14 +2,23 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const http = require('http')
+const mongoose = require("mongoose");
 
 const app = express()
 const server = http.createServer(app)
+
+async function connect() {
+    await mongoose.connect('mongodb://localhost:27017/amazon_clone')
+    console.log('Connected to database')
+}
+
+connect().catch(err => console.log(err));
 
 app.use(morgan('dev'))
 app.use(bodyParser.json({
     extended: true
 }));
+
 
 app.get('/', (req, res) => {
     res.json({
